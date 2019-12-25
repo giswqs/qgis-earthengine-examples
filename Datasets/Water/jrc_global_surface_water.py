028979aad157d0e5abd701e8ed97a78aa46458e8
Qiusheng Wu <giswqs@gmail.com>
2019-12-25 18:04:00 -0500

    Add water datasets

--- Datasets/Water/jrc_global_surface_water.py
+++ Datasets/Water/jrc_global_surface_water.py
@@ -1,6 +1,6 @@
 import ee
 from ee_plugin import Map
-dataset = ee.Image('JRC/GSW1_0/GlobalSurfaceWater')
+dataset = ee.Image('JRC/GSW1_1/GlobalSurfaceWater')
 occurrence = dataset.select('occurrence');
 occurrenceVis = {'min': 0.0, 'max': 100.0, 'palette': ['ffffff', 'ffbbbb', '0000ff']}
 Map.setCenter(59.414, 45.182, 6)
